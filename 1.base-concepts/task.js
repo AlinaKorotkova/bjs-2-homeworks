0a97e03e55dead8762ad429962c1512cc746785c
Alina <113042968+AlinaKorotkova@users.noreply.github.com>
2023-04-16 15:53:45 +0300

    Add files via upload
    Задание №1 Основные понятия

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let D = (b ** 2) - (4 * a * c);
+       if (D < 0) {
+            arr.push();
+       } else if (D == 0) {
+          let quadraticRoots = -b/(2*a);
+              arr.push(quadraticRoots);
+       } else if (D > 0){
+    let tmp = [];
+          let firstRoot = (-b + Math.sqrt(D)) / (2 * a);
+          let secondRoot = (-b - Math.sqrt(D)) / (2 * a);
+               arr.push(firstRoot, secondRoot);
+  }
+   return arr;
 }
