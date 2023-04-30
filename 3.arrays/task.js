83aa2987e76f5a83e02a42e4ad0a973b734d795b
Alina <113042968+AlinaKorotkova@users.noreply.github.com>
2023-04-30 21:46:20 +0300

    Add files via upload
    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
+//Задание 1
 function compareArrays(arr1, arr2) {
-  
+    let result = (arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]));
+    return result;
 }
 
+//Задание 2
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter((user) => gender === user.gender).map(userAge => userAge.age).reduce((acc, item, index, userAge) => acc + item/userAge.length,0)
+    return result 
 }
